548bdb6358dab08a8c2e3cf41471d1c1b88f854b
yaneldodoo <yaneldodoo@gmail.com>
2026-05-10 15:32:23 +0300

    update int payment

--- maketou_dbt/models/intermediate/int_payment.sql
+++ maketou_dbt/models/intermediate/int_payment.sql
@@ -1,10 +1,10 @@
 select 
- cast("montant" as integer) as montant,
+ coalesce(cast("montant" as integer), 0) as montantcommande,
     date_paiement,
-    statut_paiement,
-    devise_paiement,
-    erreur_paiement,
-    processeur,
-    methode_paiement,
-    passerelle_paiement
+    coalesce(statut_paiement, 'unknown') as statut_paiement,
+    coalesce(devise_paiement, 'unknown') as devise_paiement,
+    coalesce(erreur_paiement, 'unknown') as erreur_paiement,
+    coalesce(processeur, 'unknown') as processeur,
+    coalesce(methode_paiement, 'unknown') as methode_paiement,
+    coalesce(passerelle_paiement, 'unknown') as passerelle_paiement
 from {{ ref('stg_payment') }}
